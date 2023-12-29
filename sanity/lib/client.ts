b7f45e33f0d58331ca1fdef7bae30d410a620207
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-29 12:10:11 +0200

    t

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skWTcP38dCRM3PFFnZkiacAAluEU2Khqca3aoA9siTz5VQ3GAoYDPUHzEICEEF3PAbDlyOdBwubJZDa1633HbYJvDm2AMvH62yAqyKzQ1qISrfC8DnYzvsbKgdHqmmLf6fkYeLUsuLaJyABPCC7bI3T29ggHbHFGVvhItHDICp7EMYw9J9RD",
 })
